61623410532fd0546052228d9802aa226a6f0fab
Maxime Chopin <mchopin@f0r5s17.codam.nl>
2025-12-02 21:20:12 +0100

    Initial commit

---
+++ get_next_line.h
@@ -0,0 +1,19 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   get_next_line.h                                    :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: mchopin <mchopin@student.codam.nl>           +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2025/12/02 20:09:23 by mchopin       #+#    #+#                 */
+/*   Updated: 2025/12/02 20:16:54 by mchopin       ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE.H
+# define GET_NEXT_LINE .H
+# include <stdio.h>
+
+//functions I use go here
+
+#endif
