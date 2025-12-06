5f7609e27a7a7d05ea392ab277006d68079e5cac
Maxime Chopin <mchopin@f0r6s8.codam.nl>
2025-12-06 21:59:22 +0100

    small update

--- get_next_line.c
+++ get_next_line.c
@@ -5,8 +5,8 @@
 /*                                                     +:+                    */
 /*   By: mchopin <mchopin@student.codam.nl>           +#+                     */
 /*                                                   +#+                      */
-/*   Created: 2025/12/02 20:09:17 by mchopin       #+#    #+#                 */
-/*   Updated: 2025/12/06 18:11:02 by mchopin       ########   odam.nl         */
+/*   Created: 2025/12/06 21:36:04 by mchopin       #+#    #+#                 */
+/*   Updated: 2025/12/06 21:50:26 by mchopin       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,11 +14,13 @@
 
 char	*get_next_line(int fd)
 {
+	{
+	}
 	// read text file, if repeated calls with loop,
 	//	do line by line. can select file descriptor
 	// return line that was read
 	// if nothing left to read return or error return null
-	// make it worth with stdinput and reading from file
+	// make it worth with stdinput eeande reading from file
 	// returned line needs to include a terminating \n,
 	//	except for the end which doesn't need ones
 	// read file descriptor
@@ -34,8 +36,8 @@ int	main(void)
 	char buffer[256];
 	int chars_read;
 
-	fd = open("dingus.txt", O_RDONLY);
-	while ((chars_read = read(fd, buffer, 10)))
+	fd = open("bible.txt", O_RDONLY);
+	while ((chars_read = read(fd, buffer, 100)))
 	{
 		buffer[chars_read] = '\0';
 		printf("buffer : %s\n", buffer);
