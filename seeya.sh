2dd783ec28ff2b17f4ff8adc76ea618076d39b22
Maxime Chopin <mchopin@f0r4s22.codam.nl>
2025-12-07 16:39:18 +0100

    small update

--- seeya.sh
+++ seeya.sh
@@ -8,9 +8,12 @@ sleep 2
 clear
 tree
 git status
-sleep 5
+sleep 3.5
 clear
-cowsay -f eyes "ALL DONE !!! \nYou fat bastard, get outta here ;) "
+cowsay -f eyes "ALL DONE !!! You fat bastard, get outta here ;) "
 sleep 5
 clear
 cowsay -f eyes "Come on go away..."
+sleep 3
+clear
+cowsay -f dragon-and-cow "getcho bingjao ass tf outta here"
