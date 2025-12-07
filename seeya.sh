b614c53b2e7f88e28671d30098deebe0bbdf49e1
Maxime Chopin <mchopin@f0r4s22.codam.nl>
2025-12-07 16:30:41 +0100

    small update

--- seeya.sh
+++ seeya.sh
@@ -4,9 +4,9 @@ git add .
 git commit -m "small update"
 git push
 git push github master
-sleep 0.5
+sleep 2
 clear
 tree
-sleep 0.5
+sleep 2
 clear
 cowsay -f eyes "you fat bastard, get outta here ;) " 
