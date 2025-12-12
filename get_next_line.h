e6fa1bef930404102378c4138aca429239c89bd7
Maxime Chopin <mchopin@f0r6s15.codam.nl>
2025-12-12 18:19:11 +0100

    I almost uploaded with one norminette error, space instead of tab in the .h what a save I am so happy

--- get_next_line.h
+++ get_next_line.h
@@ -27,6 +27,6 @@ char	*ft_strchr(const char *str, char c);
 char	*ft_strjoin(const char *s1, const char *s2);
 char	*ft_substr(char const *s, unsigned int start, size_t len);
 char	*ft_strdup(const char *s);
-char *get_next_line(int fd);
+char	*get_next_line(int fd);
 
 #endif
